d127c6cd111b56a43428a5a2f4f157d358f6f305
Hephaestus <thereadingmachine@gmail.com>
2021-12-11 19:48:24 -0800

    Small redesign of Network. Fixing generation of Connections

--- net.go
+++ net.go
@@ -8,75 +8,90 @@ import (
 )
 
 type NeuralNet struct {
-	inputs      int
+	inputs      []*neuron
 	connections []*synapse
-	neurons     []*neuron
+	neurons     [][]*neuron
 	outputs     []*neuron
 }
 
 //NewNet Standard feed-forward network is generated
-func NewNet(inputSize int, layerCount int, layerSize int, outputSize int) (NeuralNet, error) {
+func NewNet(inputSize int, layerCount int, perLayer int, outputSize int) (NeuralNet, error) {
 	rand.Seed(time.Now().UnixNano())
 	if inputSize <= 0 || outputSize <= 0 {
-		return NeuralNet{}, fmt.Errorf("Invalid input/output size, must be greater than zero")
+		return NeuralNet{}, fmt.Errorf("Invalid input/output size, must be greater than zero!")
 	}
 
-	var nodes []*neuron
-	var connections []*synapse
+	var in []*neuron
+	for i := 0; i < inputSize; i++ {
+		in = append(in, NewNeuronP(-1))
+	}
 
+	var connections []*synapse
 	var structure [][]*neuron //only used to make life easier when adding all the connections
 
 	//Create required Neurons
 	nextId := 0
 	for i := 0; i < layerCount; i++ {
 		structure = append(structure, []*neuron{})
-		for j := 0; j < layerSize; j++ {
-			temp := &neuron{
-				nextId,
-				0,
-				i,
-				false,
-			}
-			nodes = append(nodes, temp)
-			structure[i] = append(structure[i], temp)
+		for j := 0; j < perLayer; j++ {
+			structure[i] = append(structure[i], NewNeuronP(nextId))
 			nextId++
 		}
 	}
 
-	//Connect each layer
-	for i := 0; i < len(nodes); i++ {
-		neuron := nodes[i]
-		if neuron.layer > 0 {
-			for j := 0; j < layerSize; j++ {
-				temp := &synapse{
-					structure[neuron.layer-1][j],
-					neuron,
-					rand.Float64() / math.MaxFloat64,
+	//Connect each layer, n^3 complexity... room for optimization
+	//TODO: Use Parallel computing to generate each layer
+	for i := 0; i < layerCount; i++ {
+		for l := 0; l < perLayer; l++ {
+			neuron := structure[i][l]
+
+			if i-1 >= 0 { //Connect to previous layer
+				for j := 0; j < perLayer; j++ {
+					connections = append(connections, NewSynapseP(structure[i-1][j], neuron, rand.Float64()/math.MaxFloat64))
 				}
-				connections = append(connections, temp)
+			} else { //Connect to inputs
+				for j := 0; j < inputSize; j++ {
+					connections = append(connections, NewSynapseP(in[j], neuron, rand.Float64()/math.MaxFloat64))
+				}
 			}
 		}
 	}
 
-	//Finish with Outputs
+	//Finish with Outputs, and Connect them
 	var out []*neuron
-	out = append(out, &neuron{
-		-1,
-		0,
-		-1,
-		false,
-	})
-	out = append(out, &neuron{
-		-1,
-		0,
-		-1,
-		false,
-	})
+	for i := 0; i < outputSize; i++ {
+		temp := NewNeuronP(-1)
+		in = append(out, temp)
+		for j := 0; j < perLayer; j++ {
+			connections = append(connections, NewSynapseP(structure[len(structure)-1][j], temp, rand.Float64()/math.MaxFloat64))
+		}
+	}
 
 	return NeuralNet{
-		inputSize,
+		in,
 		connections,
-		nodes,
+		structure,
 		out,
 	}, nil
+}
+
+//FeedForward Feeds the inputs forward
+func (n *NeuralNet) FeedForward(inputs ...float64) error {
+	if len(inputs) != len(n.inputs) {
+		return fmt.Errorf("Input amount did not match network input size.")
+	}
+
+	//Load into inputs
+	for i := 0; i < len(n.inputs); i++ {
+		n.inputs[i].sum = inputs[i]
+	}
+
+	//Find each connection, and process
+	for i := 0; i < len(n.neurons); i++ {
+		layer := n.neurons[i]
+		for j := 0; j < len(layer); j++ {
+
+		}
+	}
+	return nil
 }
