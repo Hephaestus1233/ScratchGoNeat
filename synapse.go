9204dcf37ea4db56d9d891130d5e7572f76c6ee2
Hephaestus <thereadingmachine@gmail.com>
2021-12-11 19:48:47 -0800

    New Constructor

--- synapse.go
+++ synapse.go
@@ -6,6 +6,14 @@ type synapse struct { //fancy for connection
 	weight float64
 }
 
+func NewSynapseP(f *neuron, t *neuron, w float64) *synapse {
+	return &synapse{
+		f,
+		t,
+		w,
+	}
+}
+
 func (s *synapse) process() {
 	s.from.activationFunction()
 	s.to.sum += (s.from.sum * s.weight)
